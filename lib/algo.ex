c93fdd5b030abb249cfeedc2d8e07deea7e482bd
Dmitry Belousov <dbelousov@defytrading.com>
2020-11-26 20:27:04 +0800

    Remove trailing spaces; force 1 newline between functions

--- lib/algo.ex
+++ lib/algo.ex
@@ -1,7 +1,6 @@
 defmodule Algo do
   use GenServer
 
-
   defstruct target_instrument: nil,
             qty_limit:         nil,
             gateway_pid:       nil,
@@ -27,7 +26,7 @@ defmodule Algo do
     {:ok, state}
   end
 
-  # Store market data and go , if the instrument is the same
+  # Store market data and go, if the instrument is the same
   @impl true
   def handle_cast(bbo = %BBO{instrument: instrument}, state = %Algo{target_instrument: target_instrument}) 
     when instrument == target_instrument
@@ -38,13 +37,12 @@ defmodule Algo do
     {:noreply, new_state}
   end
 
-
   # Handle place and cancel order
   @impl true
-  def handle_cast(%Model{FV: fv, instrument: instrument}, state = %{target_instrument: target_instrument}) 
+  def handle_cast(%Model{FV: fv, instrument: instrument}, state = %{target_instrument: target_instrument})
     when instrument == target_instrument
   do
-    IO.puts("FV : #{fv}")    
+    IO.puts("FV : #{fv}")
     values = get_values(fv, state.bbo, state.inventory, state.qty_limit)
     values |> IO.inspect()
     order_price = Map.keys(state.orders)
@@ -69,7 +67,7 @@ defmodule Algo do
     IO.puts("Cancel Out of depth order")
     values_price = Enum.map(values, fn x -> elem(x, 0) end)
     cancel       = Enum.filter(order_price, fn x -> x not in values_price end) |> IO.inspect()
-    
+
     Enum.each(cancel, fn x ->
       GenServer.cast(state.gateway_pid, {:cancel, %{
         price: state.orders[x].price,
@@ -77,47 +75,47 @@ defmodule Algo do
         code:  nil
       }})
     end)
-    
-    order_list = 
-      Enum.into(cancel, state.orders, fn x -> 
-        {x, %{state.orders[x] | state: :pending_cancel, code: nil}} 
+
+    order_list =
+      Enum.into(cancel, state.orders, fn x ->
+        {x, %{state.orders[x] | state: :pending_cancel, code: nil}}
       end)
     state      = Map.put(state, :orders, order_list)
-    
+
     # 2. Cancel order according to Bid Ask valuation
-    # Cancel Bid 
+    # Cancel Bid
     IO.puts("Cancel Bid")
-    cancel = 
-      values 
+    cancel =
+      values
         |>  Enum.filter(fn {price, _, _, _} -> price in order_price end)
         |>  Enum.filter(fn {_, v_bid, _, _} -> v_bid <= state.margin end)
         |>  Enum.filter(fn {_, _, _,  side} -> side == :bid end)
         |>  Enum.filter(fn {price, _, _, _} -> :pending_cancel != state.orders[price][:state] end)
         |>  IO.inspect()
-    Enum.each(cancel, fn x -> 
+    Enum.each(cancel, fn x ->
       GenServer.cast(state.gateway_pid, {:cancel, %{
         price: state.orders[elem(x,0)].price,
         state: :pending_cancel,
         code:  nil
       }})
       end)
-    
-    order_list = 
-      Enum.into(cancel, state.orders, fn x -> {elem(x, 0), 
+
+    order_list =
+      Enum.into(cancel, state.orders, fn x -> {elem(x, 0),
         %{state.orders[elem(x, 0)] | state: :pending_cancel, code: nil}}
       end)
     state = Map.put(state, :orders, order_list)
 
-    # Cancel Ask 
+    # Cancel Ask
     IO.puts("Cancel Ask")
-    cancel = 
+    cancel =
       values
       |>  Enum.filter(fn {price, _, _, _} -> price in order_price end)
       |>  Enum.filter(fn {_, _, v_ask, _} -> v_ask <= state.margin end)
       |>  Enum.filter(fn {_, _, _,  side} -> side == :ask end)
       |>  Enum.filter(fn {price, _, _, _} -> :pending_cancel != Map.get(state.orders[price], :state) end)
       |>  IO.inspect()
-    Enum.each(cancel, fn x -> 
+    Enum.each(cancel, fn x ->
       GenServer.cast(state.gateway_pid, {:cancel, %{
         price: state.orders[elem(x,0)].price,
         state: :pending_cancel,
@@ -125,15 +123,15 @@ defmodule Algo do
       }})
     end)
 
-    order_list = 
-      Enum.into(cancel, state.orders, fn x -> {elem(x, 0), 
+    order_list =
+      Enum.into(cancel, state.orders, fn x -> {elem(x, 0),
         %{state.orders[elem(x, 0)] | state: :pending_cancel, code: nil}}
       end)
     state = Map.put(state, :orders, order_list)
-    
+
     # 3. Cancel Order if the order is at the wrong side
     IO.puts("Cancel wrong side order ")
-    cancel = 
+    cancel =
       values
         |>  Enum.filter(fn {price, _, _, _   } -> price in order_price end)
         |>  Enum.filter(fn {price, _, _, side} -> side != Map.get(state.orders[price], :side) end)
@@ -146,64 +144,63 @@ defmodule Algo do
         code:  nil
       }})
       end)
-    
-    order_list = 
-      Enum.into(cancel, state.orders, fn x -> {elem(x, 0), 
+
+    order_list =
+      Enum.into(cancel, state.orders, fn x -> {elem(x, 0),
         %{state.orders[elem(x, 0)] | state: :pending_cancel, code: nil}}
       end)
     state = Map.put(state, :orders, order_list)
-    
+
     # Placing order
     # If values > margin and there isn't any order at that price, then place order
     # Bid case
     IO.puts("Place bid orders")
-    place = 
-    values  
+    place =
+    values
     |>  Enum.filter(fn {_, v_bid, _, _} -> v_bid > state.margin end)
     |>  Enum.filter(fn {price, _, _, _} -> :active != state.orders[price][:state] end)
     |>  Enum.filter(fn {price, _, _, _} -> :pending_active != state.orders[price][:state] end)
     |>  IO.inspect()
-    Enum.each(place, fn x -> 
-      GenServer.cast(state.gateway_pid, {:place, 
+    Enum.each(place, fn x ->
+      GenServer.cast(state.gateway_pid, {:place,
         %{order | price: elem(x, 0), side: elem(x, 3), state: :pending_active}})
     end)
-    
-    order_list = 
-      Enum.into(place, state.orders, fn x -> {elem(x, 0), 
-        %{order | price: elem(x, 0), side: elem(x, 3), state: :pending_active}} 
+
+    order_list =
+      Enum.into(place, state.orders, fn x -> {elem(x, 0),
+        %{order | price: elem(x, 0), side: elem(x, 3), state: :pending_active}}
       end)
     state = Map.put(state, :orders, order_list)
 
     #Ask Case
     IO.puts("Place ask orders")
-    place = 
-    values  
+    place =
+    values
     |>  Enum.filter(fn {_, _, v_ask, _} -> v_ask > state.margin end)
     |>  Enum.filter(fn {price, _, _, _} -> :active != state.orders[price][:state] end)
     |>  Enum.filter(fn {price, _, _, _} -> :pending_active != state.orders[price][:state] end)
     |>  IO.inspect()
-    Enum.each(place, fn x -> 
-      GenServer.cast(state.gateway_pid, {:place, 
+    Enum.each(place, fn x ->
+      GenServer.cast(state.gateway_pid, {:place,
         %{order | price: elem(x, 0), side: elem(x, 3), state: :pending_active}})
     end)
 
-    order_list = 
-      Enum.into(place, state.orders, fn x -> {elem(x, 0), 
-        %{order | price: elem(x, 0), side: elem(x, 3), state: :pending_active}} 
+    order_list =
+      Enum.into(place, state.orders, fn x -> {elem(x, 0),
+        %{order | price: elem(x, 0), side: elem(x, 3), state: :pending_active}}
       end)
     state = Map.put(state, :orders, order_list)
-
     state |> IO.inspect()
+
     {:noreply, state}
   end
-
 
   # If the instrument are not the same, then do nothing
   @impl true
   def handle_cast(%BBO{}, state) do
     {:noreply, state}
   end
-    
+
   # State update for active orders, now order is a list of order
   @impl true
   def handle_cast({:state_update, feedback = %{state: :active}}, state) do
@@ -211,7 +208,7 @@ defmodule Algo do
     new_order = %{feedback.order | state: feedback.state, code: feedback.code} 
     order_map = Map.put(state.orders, new_order.price, new_order) 
     new_state = Map.put(state, :orders, order_map) |> IO.inspect()
-    
+
     {:noreply, new_state}
   end
 
@@ -236,19 +233,19 @@ defmodule Algo do
       :ask  -> state.inventory - fill.qty
     end
     state = Map.put(state, :inventory, inventory)
-    
+
     order_map = Map.delete(state.orders, feedback.order.price)
     new_state = Map.put(state, :orders, order_map) |> IO.inspect()
 
     {:noreply, new_state}
   end
-  
+
   # Handle Partially Filled Order, update inventory, update values
   @impl true
   def handle_cast({:fill, %{fill: fill, order: order}}, state) do
     IO.puts("Order Filled")
     # Adjust state inventory
-    inventory = 
+    inventory =
     case fill.side do
       :bid  -> state.inventory + fill.qty
       :ask  -> state.inventory - fill.qty
@@ -277,30 +274,29 @@ defmodule Algo do
   # Calculate expected values for each prices
   # Creates a list of tuples {price, value_bid, value_ask, side}
   def get_values(fv, md, inv, qty_limit) do
-    bid_price = 
-      md.bids 
-        |>  Enum.unzip() 
-        |>  elem(0) 
+    bid_price =
+      md.bids
+        |>  Enum.unzip()
+        |>  elem(0)
         |>  Enum.map(fn p -> {
               p,
-              fv - p - inv / qty_limit, 
+              fv - p - inv / qty_limit,
               -1 * (fv - p - inv / qty_limit),
               :bid
-              } 
+              }
             end)
 
-    ask_price = 
+    ask_price =
       md.asks
-        |>  Enum.unzip() 
-        |>  elem(0) 
+        |>  Enum.unzip()
+        |>  elem(0)
         |>  Enum.map(fn p -> {
               p,
-              fv - p - inv / qty_limit, 
+              fv - p - inv / qty_limit,
               -1 * (fv - p - inv / qty_limit),
-              :ask} 
+              :ask}
             end)
 
     bid_price ++ ask_price
-  end 
+  end
 end
-
