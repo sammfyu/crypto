e8fdffd76b1388c20fec26ca184e6d610df000df
Sam Yu <samyu@defytrading.com>
2020-12-11 03:43:07 +0900

    Simplify Place & Cancel function

--- lib/order_management.ex
+++ lib/order_management.ex
@@ -1,14 +1,10 @@
 defmodule OrderManagement do
-  def cancel(valuations, orders, margin, gateway_pid, inventory, qty_limit) do
+  def cancel(valuations, orders, gateway_pid) do
     orders
     |> Enum.map(fn
       {{_, side} = key, %Order{state: :active} = order} ->
         order = case Map.get(valuations, key) do
-          value when is_nil(value) or value <= margin or inventory <= -qty_limit and side == :ask ->
-            order = %{order | state: :pending_cancel}
-            GenServer.cast(gateway_pid, {:cancel, order})
-            order
-          value when is_nil(value) or value <= margin or inventory >= qty_limit and side == :bid ->
+          nil ->
             order = %{order | state: :pending_cancel}
             GenServer.cast(gateway_pid, {:cancel, order})
             order
@@ -22,39 +18,11 @@ defmodule OrderManagement do
     |> Map.new
   end
 
-  def place(valuations, orders, margin, gateway_pid, book_depth, {bid_depth, ask_depth}, book_printing_qty, target_instrument, inventory, qty_limit) do
+  def place(valuations, orders, gateway_pid, book_printing_qty, target_instrument) do
     valuations
     |> Enum.reduce(orders, fn {{price, side} = key, value}, orders ->
-      case {
-        Map.get(orders, key),
-        Enum.count(orders, fn {{_,s}, %{state: state}} -> s == side and state != :pending_cancel end),
-      }
-      do
-        {nil, count} when value > margin and count < book_depth and price > bid_depth and price < ask_depth ->
-          order = %Order{
-            price:      price,
-            qty:        book_printing_qty,
-            side:       side,
-            instrument: target_instrument,
-            type:       :limit,
-            state:      :pending_active
-          }
-          GenServer.cast(gateway_pid, {:place, order})
-          Map.put(orders, {price, side}, order)
-        # Place order only on the bid side, becaise we sold too much
-        {nil, count} when value > margin and count < book_depth and inventory <= -qty_limit and side == :bid and price > bid_depth and price < ask_depth ->
-          order = %Order{
-            price:      price,
-            qty:        book_printing_qty,
-            side:       side,
-            instrument: target_instrument,
-            type:       :limit,
-            state:      :pending_active
-          }
-          GenServer.cast(gateway_pid, {:place, order})
-          Map.put(orders, {price, side}, order)
-        # Place order only on the ask side, because we bought too much
-        {nil, count} when value > margin and count < book_depth and inventory >= qty_limit and side == :ask and price > bid_depth and price < ask_depth ->
+      case Map.get(orders, key) do
+        nil ->
           order = %Order{
             price:      price,
             qty:        book_printing_qty,
