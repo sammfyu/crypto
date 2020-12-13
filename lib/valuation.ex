00b28192fd2c165de959710e85e4be4d47293fa0
Sam Yu <samyu@defytrading.com>
2020-12-14 03:34:35 +0900

    Add variable that prevents price of orders to self-oscillate

--- lib/valuation.ex
+++ lib/valuation.ex
@@ -1,18 +1,27 @@
 defmodule Valuations do
-  def get(fv, %{bids: bids, asks: asks}, inv, lim, margin, depth) do
-    {bids, _} = Enum.reduce_while(bids, {%{}, depth}, fn
-      _, {bids, depth}            when depth == 0 or inv >= lim           -> {:halt, {bids, depth}}
-      {price, qty}, {bids, depth} when (fv - price - inv / lim) >= margin -> {:cont, {Map.put(bids, {price, :bid}, qty), depth - 1}}
-      _, {bids, depth}                                                    -> {:cont, {bids, depth}}
+  def get(fv, %{bids: bids, asks: asks}, inv, d_inv, lim, margin, depth, tick) do
+    bid_adj = inv >= 0 and d_inv < 0
+    ask_adj = inv <= 0 and d_inv > 0
+    {bid, _, _} = Enum.reduce_while(Enum.sort(bids ++ asks, :desc), {%{}, depth, bid_adj}, fn
+      _,            {bid, depth, bid_adj} when depth == 0 and bid_adj == false or inv >= lim                   -> {:halt, {bid, depth, bid_adj}}
+      {price,   _}, {bid, depth, bid_adj} when ((fv - price) / tick - inv / lim) >= margin and bid_adj == true -> {:cont, {bid, depth, false}}
+      {price, qty}, {bid, depth, bid_adj} when ((fv - price) / tick - inv / lim) >= margin                     -> {:cont, {Map.put(bid, {price, :bid}, qty), depth - 1, bid_adj}}
+      _,            {bid, depth, bid_adj}                                                                      -> {:cont, {bid, depth, bid_adj}}
     end)
 
-    {asks, _} = Enum.reduce_while(asks, {%{}, depth}, fn
-      _, {asks, depth}            when depth == 0 or inv <= -1 * lim           -> {:halt, {asks, depth}}
-      {price, qty}, {asks, depth} when -1 * (fv - price - inv / lim) >= margin -> {:cont, {Map.put(asks, {price, :ask}, qty), depth - 1}}
-      _, {asks, depth}                                                         -> {:cont, {asks, depth}}
+    {ask, _, _} = Enum.reduce_while(Enum.sort(bids ++ asks, :asc) , {%{}, depth, ask_adj}, fn
+      _,            {ask, depth, ask_adj} when depth == 0 and ask_adj == false or inv <= -1 * lim                   -> {:halt, {ask, depth, ask_adj}}
+      {price,   _}, {ask, depth, ask_adj} when -1 * ((fv - price) / tick - inv / lim) >= margin and ask_adj == true -> {:cont, {ask, depth, false}}
+      {price, qty}, {ask, depth, ask_adj} when -1 * ((fv - price) / tick - inv / lim) >= margin                     -> {:cont, {Map.put(ask, {price, :ask}, qty), depth - 1, ask_adj}}
+      _,            {ask, depth, ask_adj}                                                                           -> {:cont, {ask, depth, ask_adj}}
+
     end)
 
-    Map.merge(bids, asks)
+    Map.merge(bid, ask)
   end
-
 end
+
+
+
+
+
--- test/valuation_test.exs
+++ test/valuation_test.exs
@@ -6,9 +6,11 @@ defmodule ValuationTest do
     # Book_depth = 2, return correct book_depth
     fv         = 99.5
     inventory  = 0
+    d_inv      = 0
     qty_limit  = 10
     margin     = 0.1
     book_depth = 2
+    tick       = 1
     md = %BBO{
       bids: [{99,  10}, {98,  10}, {97, 10}, {96, 5}, {95, 4}],
       asks: [{100, 10}, {101, 12}, {102, 5}, {103,16}, {104, 22}],
@@ -16,7 +18,7 @@ defmodule ValuationTest do
     }
     # Function output : Map in form %{{price, side} => qty, ... }
     # Return correct depth of the book
-    assert Valuations.get(fv, md, inventory, qty_limit, margin, book_depth) == %{
+    assert Valuations.get(fv, md, inventory, d_inv, qty_limit, margin, book_depth, tick) == %{
       {99,  :bid } => 10,
       {98,  :bid } => 10,
       {100, :ask } => 10,
@@ -26,48 +28,51 @@ defmodule ValuationTest do
     # case 2
     # Inventory reached hard limit. Oversold
     # Place only bid side order, place bid@99, bid@100
-    # To avoid bid@100 self-trade with ask@100
-    # Drop bid@100 and place bid@98 to fill up book_depth
     fv         = 99.5
     inventory  = -10
+    d_inv      = -10
     qty_limit  = 10
     margin     = 0.1
     book_depth = 2
-    assert Valuations.get(fv, md, inventory, qty_limit, margin, book_depth) == %{
-      {99,  :bid } => 10,
-      {98,  :bid } => 10
+    tick       = 1
+    assert Valuations.get(fv, md, inventory, d_inv, qty_limit, margin, book_depth, tick) == %{
+      {99,   :bid } => 10,
+      {100,  :bid } => 10
     }
 
     # case 3
     # Handle Self-Trading problem
     # inv = 8 and we should move our orders down
-    # ask@99 willnot be placed to avoid self-trade
     fv         = 99.5
     inventory  = 8
+    d_inv      = 8
     qty_limit  = 10
     margin     = 0.1
     book_depth = 3
-    assert Valuations.get(fv, md, inventory, qty_limit, margin, book_depth) == %{
+    tick       = 1
+    assert Valuations.get(fv, md, inventory, d_inv, qty_limit, margin, book_depth, tick) == %{
       {98,  :bid} => 10,
       {97,  :bid} => 10,
       {96,  :bid} => 5,
+      {99,  :ask} => 10,
       {100, :ask} => 10,
       {101, :ask} => 12,
-      {102, :ask} => 5
     }
 
     # case 4
     # Handle market moves, this will cancel ask@100, place bid@100
-    # To avoid self-trade, bid@100 will not be placed until next BBO update
+    # No need to avoid self trade. Ignore inv adjustment condition and place aggressive order
     fv         = 101
     inventory  = 0
+    d_inv      = 0
     qty_limit  = 10
     margin     = 0.1
     book_depth = 3
-    assert Valuations.get(fv, md, inventory, qty_limit, margin, book_depth) == %{
-      {97,  :bid} => 10,
+    tick       = 1
+    assert Valuations.get(fv, md, inventory, d_inv, qty_limit, margin, book_depth, tick) == %{
       {98,  :bid} => 10,
       {99,  :bid} => 10,
+      {100, :bid} => 10,
       {102, :ask} => 5,
       {103, :ask} => 16,
       {104, :ask} => 22
@@ -77,10 +82,33 @@ defmodule ValuationTest do
     # return nothing for empty order book
     fv         = 101
     inventory  = 0
+    d_inv      = 0
     qty_limit  = 10
     margin     = 0.1
     book_depth = 6
+    tick       = 1
     md = %{bids: [], asks: [], instrument: nil}
-    assert Valuations.get(fv, md, inventory, qty_limit, margin, book_depth) == %{}
+    assert Valuations.get(fv, md, inventory, d_inv, qty_limit, margin, book_depth, tick) == %{}
+
+    # case 6
+    # Adjust price to prevent osillation
+    # Assume we had 99/100 -> fill ask 8 lots -> 100/101 -> fill bid 7 lots > 99/100
+    # inv = -1 but d_inv = 7, adjust bid such that -> 99/101
+    md = %BBO{
+      bids: [{99,  10}, {98,  10}, {97, 10}, {96, 5}, {95, 4}],
+      asks: [{100, 10}, {101, 12}, {102, 5}, {103,16}, {104, 22}],
+      instrument: "BTCCQ"
+    }
+    assert Valuations.get(99.5, md, -1, 7, 10, 0.1, 1, 1) == %{
+      {99, :bid} => 10,
+      {101,:ask} => 12
+    }
+
+    # case 7
+    assert Valuations.get(99.5, md, 0, 7, 10, 0.1, 1, 1) == %{
+      {99, :bid} => 10,
+      {101,:ask} => 12
+    }
+
   end
 end
